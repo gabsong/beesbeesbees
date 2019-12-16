0453bf29e6dd1061f0b3f9bfcf987a4ae36d686b
Gabriel Song <gabsong4@gmail.com>
2019-12-16 11:42:44 -0800

    Implement superclasses and subclasses in pseudoclassical style

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+  // calls Grub() and the instance of Bee will be bound to the keyword 'this'
+  // within the body of Grub
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
